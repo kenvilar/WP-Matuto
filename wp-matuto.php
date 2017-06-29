de26ec6980eded49b2e8033d7028e2f9f13fcef5
Ken Vilar <kenvilar@Deestur.local>
2017-06-29 12:20:23 +0800

    Upload main plugin file

---
+++ wp-matuto.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WP Matuto
+Plugin URI: http://kenvilar.com/
+Description: A brief description of the Plugin.
+Version:     0.0.1
+Author:      Ken Vilar
+Author URI:  http://kenvilar.com/
+Text Domain: wpmatuto
+Domain Path: /languages
+License:     GPL2
+
+WP Matuto is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+WP Matuto is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WP Matuto. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
