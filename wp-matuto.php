01d355ffdcfd99a3c93993e533643c22b2f59541
Ken Vilar <kenvilar@gmail.com>
2017-07-03 23:13:39 +0800

    Liberal spacing for human readability - coding standards

--- wp-matuto.php
+++ wp-matuto.php
@@ -24,25 +24,25 @@ You should have received a copy of the GNU General Public License
 along with WP Matuto. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
-/* This is the function where the password is generated */
-if(!function_exists('wpmatuto_generate_password')):
-    function wpmatuto_generate_password($extra_special_chars = false) {
-        $chars = 'abcdefghijklmnopqrstuvwxyz';
+// This is the function where the password is generated
+if ( ! function_exists( 'wpmatuto_generate_password' ) ):
+    function wpmatuto_generate_password( $extra_special_chars = false ) {
+        $chars  = 'abcdefghijklmnopqrstuvwxyz';
         $chars .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $chars .= '0123456789';
         $chars .= '!@#$%^&*()';
 
-        if($extra_special_chars):
+        if ( $extra_special_chars ):
             $chars .= '-_ []{}<>~`+=,.;:/?|';
         endif;
 
         $wpmatuto_password = ''; // Initialize the password string
-
-        for ($i = 0; $i < 12; $i += 1) {    // 12 is the length of the password
-            $wpmatuto_password .= substr($chars, wp_rand(0, strlen($chars) - 1), 1);
+        $password_length = 12;
+        for ( $i = 0; $i < $password_length; $i += 1 ) {
+            $wpmatuto_password .= substr( $chars, wp_rand( 0, strlen( $chars ) - 1 ), 1 );
         }
 
-        return apply_filters('random_password', $wpmatuto_password);
+        return apply_filters( 'random_password', $wpmatuto_password );
     }
 endif;
 
@@ -61,38 +61,41 @@ function wpmatuto_style_password() {
     ";
 }
 
-add_action('admin_head', 'wpmatuto_style_password');
+add_action( 'admin_head', 'wpmatuto_style_password' );
 
 function wpmatuto_show_generated_password() {
     /*
      * Set the $inc_extra_special_chars to true if you want to include
-     * the extra special characters
+     * the extra special characters.
     */
-    $show_password = wpmatuto_generate_password($inc_extra_special_chars = false);
+    $show_password = wpmatuto_generate_password( $inc_extra_special_chars = false );
     // Do not use _e, just use __ when using printf or sprintf
     printf(
-        __('<p class=\'wpmatuto-show-password\'>Generated&nbsp;Password:&nbsp;<strong>%s</strong></p>', 'wpmatuto'),
+        __( '<p class=\'wpmatuto-show-password\'>Generated&nbsp;Password:&nbsp;<strong>%s</strong></p>', 'wpmatuto' ),
         $show_password
     );
 }
 
-add_action('admin_notices', 'wpmatuto_show_generated_password');
+add_action( 'admin_notices', 'wpmatuto_show_generated_password' );
 
 // Custom Taxonomy for Tutorial
 function wpmatuto_register_taxonomy_tutorial() {
-    // either use [] or array() depending on your PHP version you're using
+    /*
+     * either use [] or array() depending on your PHP version you're using.
+     * [] can be used when creating in JavaScript
+    */
     $labels = array(
-        'name'              => _x('Tutorials', 'taxonomy general name', 'wpmatuto'),
-        'singular_name'     => _x('Tutorial', 'taxonomy singular name', 'wpmatuto'),
-        'search_items'      => __('Search Tutorials', 'wpmatuto'),
-        'all_items'         => __('All Tutorials', 'wpmatuto'),
-        'parent_item'       => __('Parent Tutorial', 'wpmatuto'),
-        'parent_item_colon' => __('Parent Tutorial:', 'wpmatuto'),
-        'edit_item'         => __('Edit Tutorial', 'wpmatuto'),
-        'update_item'       => __('Update Tutorial', 'wpmatuto'),
-        'add_new_item'      => __('Add New Tutorial', 'wpmatuto'),
-        'new_item_name'     => __('New Tutorial Name', 'wpmatuto'),
-        'menu_name'         => __('Tutorial', 'wpmatuto'),
+        'name'              => _x( 'Tutorials', 'taxonomy general name', 'wpmatuto' ),
+        'singular_name'     => _x( 'Tutorial', 'taxonomy singular name', 'wpmatuto' ),
+        'search_items'      => __( 'Search Tutorials', 'wpmatuto' ),
+        'all_items'         => __( 'All Tutorials', 'wpmatuto' ),
+        'parent_item'       => __( 'Parent Tutorial', 'wpmatuto' ),
+        'parent_item_colon' => __( 'Parent Tutorial:', 'wpmatuto' ),
+        'edit_item'         => __( 'Edit Tutorial', 'wpmatuto' ),
+        'update_item'       => __( 'Update Tutorial', 'wpmatuto' ),
+        'add_new_item'      => __( 'Add New Tutorial', 'wpmatuto' ),
+        'new_item_name'     => __( 'New Tutorial Name', 'wpmatuto' ),
+        'menu_name'         => __( 'Tutorial', 'wpmatuto' ),
     );
     $args = array(
         'hierarchical'      => true,
@@ -100,9 +103,9 @@ function wpmatuto_register_taxonomy_tutorial() {
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
-        'rewrite'           => array('slug' => 'wpmatuto-tutorial'),
+        'rewrite'           => array( 'slug' => 'wpmatuto-tutorial' ),
     );
-    register_taxonomy('wpmatuto-tutorial', array('post'), $args);
+    register_taxonomy( 'wpmatuto-tutorial', array( 'post' ), $args );
 }
 
-add_action('init', 'wpmatuto_register_taxonomy_tutorial');
+add_action( 'init', 'wpmatuto_register_taxonomy_tutorial' );
